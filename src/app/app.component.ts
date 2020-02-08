e86446502b3e2dff0ab9a41dbd9d44781760f9d8
manas-sahu <manas.sahu.one@gmail.com>
2020-02-08 23:42:17 +0530

    Reactive Form with custom validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { PasswordStrengthValidator, checkPassword } from './password.validator';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
   title = 'Epictenet';
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+  phoneNumber = "^^((\\+91-?)|0)?[0-9]{10}$";
+  epicteNetForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.pattern(this.emailPattern)]),
+    password: new FormControl('', [Validators.required, PasswordStrengthValidator]),
+    reapeatPassword: new FormControl('', [Validators.required , checkPassword]),
+    phoneNumber: new FormControl('', [Validators.required, Validators.pattern(this.phoneNumber)]),
+  });
+  showData: boolean = false;
+  getFormData(){
+    console.log(this.epicteNetForm);
+    this.showData = true;
+    
+  }
+//   public checkPasswords(control: FormGroup) { // here we have the 'passwords' group
+//   let pass = control.get('password').value;
+//   let confirmPass = control.get('reapeatPassword').value;
+
+//   return pass === confirmPass ? null : { notSame: true }     
+// }
 }
---
+++ src/app/password.validator.ts
@@ -0,0 +1,45 @@
+import { AbstractControl, ValidationErrors, FormGroup  } from "@angular/forms"
+
+export const PasswordStrengthValidator = function (control: AbstractControl): ValidationErrors | null {
+
+  console.log(control);
+  
+
+  let value: string = control.value || '';
+
+  if (!value) {
+    return null
+  }
+  let upperCaseCharacters = /[A-Z]+/g
+  if (upperCaseCharacters.test(value) === false) {
+    return { passwordStrength: `text has to contine Upper case characters,current value ${value}` };
+  }
+
+  let lowerCaseCharacters = /[a-z]+/g
+  if (lowerCaseCharacters.test(value) === false) {
+    return { passwordStrength: `text has to contine lower case characters,current value ${value}` };
+  }
+
+
+  let numberCharacters = /[0-9]+/g
+  if (numberCharacters.test(value) === false) {
+    return { passwordStrength: `text has to contine number characters,current value ${value}` };
+  }
+
+  let specialCharacters = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/
+  if (specialCharacters.test(value) === false) {
+    return { passwordStrength: `text has to contine special character,current value ${value}` };
+  }
+  return null;
+}
+
+export const checkPassword = function (control: AbstractControl): ValidationErrors | null {
+  let value: string = control.value || '';
+  if (!value) {
+    return null
+  }
+  if (control.parent.value.password !== control.value){
+    return { checkPassword: `Not matching password field,current value ${value}` };
+  }
+  return null;
+}
